029363a909bf2513721d48cf68e656219fe4dbc2
sandip Vavhal <svavhal.ror@gmail.com>
2015-11-16 17:48:47 +0530

    integrated twilio in rails 4

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render text: "Thank you! You will receive an SMS shortly with verification instructions."
+       # Instantiate a Twilio client
+      client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+
+      # Create and send an SMS message
+      client.account.sms.messages.create(
+        from: TWILIO_CONFIG['from'],
+        to: @user.phone,
+        body: "Thanks for signing up. To verify your account, please reply HELLO to this message."
+      )
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email,:phone)
+  end
+end
