f6967d9b03e2272f667482b8c0896a92b50982f7
sandip Vavhal <svavhal.ror@gmail.com>
2015-11-16 18:48:35 +0530

    removed verifications controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
@@ -7,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render text: "Thank you! You will receive an SMS shortly with verification instructions."
+      render text: "Thank you! You will receive an SMS shortly."
        # Instantiate a Twilio client
       client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
 
